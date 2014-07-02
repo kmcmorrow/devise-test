aa338284304d3b34b99bc340e7b5119351d7a1cd
Kevin McMorrow <kmcm07+github@gmail.com>
2014-07-02 17:03:23 +0100

    Add gmail smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'localhost:3000',
+  port: 587,
+  user_name: '',
+  password: '',
+  authentication: :plain
+}
+ActionMailer::Base.raise_delivery_errors = true
